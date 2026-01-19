60e94f1185572c70da746bc922b161e74e4d3b55
Bibis Gr <bertha.garrastacho@digitechfp.com>
2026-01-19 12:29:57 +0100

    feat: parte 1 Creacion de entidades y relaciones MtoM

---
+++ src/main/java/progresa/escueladama/entity/Alumno.java
@@ -0,0 +1,35 @@
+package progresa.escueladama.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "alumno")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Alumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @ManyToMany
+    @JoinTable(
+            name = "alumno_curso",
+            joinColumns = @JoinColumn(name = "alumno_id"),
+            inverseJoinColumns = @JoinColumn(name = "curso_id")
+    )
+    @JsonManagedReference
+    private Set<Curso> cursos = new HashSet<>();
+}
---
+++ src/main/java/progresa/escueladama/entity/Curso.java
@@ -0,0 +1,33 @@
+package progresa.escueladama.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "curso")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "creditos")
+    private Integer creditos;
+
+    @ManyToMany(mappedBy = "curso")
+    @JsonBackReference
+    private Set<Alumno> alumnos = new HashSet<>();
+}
