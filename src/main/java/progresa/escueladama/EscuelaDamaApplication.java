e4c50102fea552674e3fffa2d7b625dcfe2eeac8
Bibis Gr <bertha.garrastacho@digitechfp.com>
2026-01-19 12:12:54 +0100

    Initial commit

---
+++ src/main/java/progresa/escueladama/EscuelaDamaApplication.java
@@ -0,0 +1,13 @@
+package progresa.escueladama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscuelaDamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscuelaDamaApplication.class, args);
+    }
+
+}
